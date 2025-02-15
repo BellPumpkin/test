1d0263ff0aeaee6598e73e4828de42982933721b
BellPumpkin <whdgh135@gmail.com>
2025-02-15 17:18:34 +0900

    Feat: 책 디테일 페이지 기능 구현 레이아웃 작성
    문제점: params로 받을 때, Promise 객체로 받아서 타입에 관한 문제 발생

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client"
+import { usePathname, useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
   params: {
@@ -6,10 +8,41 @@ type Props = {
   }
 }
 
+type BookProps = {
+  id: string,
+  title: string,
+  author: string,
+  price: string,
+  count: number
+}
+
 export default function DetailPage({params}: Props) {
+  const [currentBookDetail, setCurrentBookDetail] = useState<BookProps | null>();
+
+  const { id } = React.use(params);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/MOCK_DATA.json');
+      const data = await response.json();
+      const test = data.filter((item: any) => (
+        item.id.toString() === id
+      ))
+      setCurrentBookDetail(test[0]);
+    };
+    fetchData();
+  }, [id]);
+
   return (
-    <>
-      {params ? <div>{params.id} DetailPage</div> : <div>Loading...</div>}
-    </>
+    <div className="flex justify-center mt-[100px]">
+      {currentBookDetail ? (
+        <div className='flex flex-col bg-blue-300 w-[200px] h-[200px] justify-center items-center'>
+          <div>제목: {currentBookDetail.title}</div>
+          <div>저자: {currentBookDetail.author}</div>
+          <div>가격: {currentBookDetail.price}</div>
+          <div>{currentBookDetail.count} 권</div>
+        </div>
+      ) : (<div>Loading...</div>)}
+    </div>
   )
 }
